fa8bccfff29cddc1e9f665de1d2302d9210a8336
canankarakoc <canankarakoc@gmail.com>
2019-04-23 16:43:33 +0200

    Modified

--- build_EDMhelper.R
+++ build_EDMhelper.R
@@ -4,10 +4,12 @@ require(roxygen2)
 #setwd("~/Dropbox/Projects/055_Karakoc_rpackage/")
 #create("EDMhelper")
 
-setwd("~/Dropbox/Projects/055_Karakoc_rpackage/EDMhelper/")
+setwd("C:/Users/karakoc/Documents/Everyfthing/r_package_EDMhelper/r_package_EDMhelper/EDMhelper/")
 document()
 check()
 
 build()
 load_all()
+
+
 
