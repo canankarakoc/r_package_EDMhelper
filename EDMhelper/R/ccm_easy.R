c82ab06d86c27c4704998625fdc5342b0e87c79f
canankarakoc <canankarakoc@gmail.com>
2019-04-23 16:28:46 +0200

    Modified ccm_easy  function

--- EDMhelper/R/ccm_easy.R
+++ EDMhelper/R/ccm_easy.R
@@ -1,6 +1,6 @@
 #' ccm_easy
 #'
-#' Perform convergent cross mapping using simplex projection.
+#' Performs convergent cross mapping using simplex projection across all pairwise comparisons.
 #' @param df A data.frame where each column is a time series. Note that additional columns such as time and replicates should not be included in this data.frame. 
 #' @param best_E The embedding dimensions to use for time delay embedding.
 #' @param lib_segments Segments which are used for predictions.
@@ -11,13 +11,12 @@
 #' @import rEDM
 #' @export
 
-ccm_easy <- function (df, best_E, lib_segments, pred_segments=lib_segments, niter=100, ...){
+ccm_easy <- function (df, best_E, lib_segments, pred_segments=lib_segments, ...){
   
   vars            <- expand.grid(lib_column = colnames(df), target_column=colnames(df))
   vars$best_E     <- unname(best_E[match(vars[,"lib_column"],names(best_E))])
   vars_matrix     <- vars[vars$lib_column!= vars$target_column,]
   vars_matrix_ord <- vars_matrix[order(vars_matrix$lib_column, vars_matrix$target_column),]
-  
   
   
  ccm_output <- do.call(rbind, lapply(seq_len(NROW(vars_matrix_ord)), function(i){
@@ -27,8 +26,8 @@ ccm_easy <- function (df, best_E, lib_segments, pred_segments=lib_segments, nite
         E=vars_matrix_ord$best_E[i],  
         lib_column = as.character(vars_matrix_ord$lib_column[i]),
         target_column = as.character(vars_matrix_ord$target_column[i]),
-        num_samples = niter,
         silent=TRUE,...)
     }))
 
+}
 
