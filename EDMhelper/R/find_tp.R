4e6eebbafe1cb5f2fa966513ef86b65951eedeba
atclark <adam.tclark@gmail.com>
2019-04-24 13:59:51 +0200

    added and updated findtp function

--- EDMhelper/R/find_tp.R
+++ EDMhelper/R/find_tp.R
@@ -1,7 +1,7 @@
 #' find_tp
 #'
 #' Finds the prediction lag tp corresponding to a convergent cross mapping significance test.
-#' @param pval output of a call from "ccm_significance".
+#' @param sgn output of a call from "ccm_significance".
 #' @param cutoff cuttoff for p-value. Default is 0.05.
 #' @keywords rEDM, prediction lag
 #' @return Data frame with the tps corresponding to the best predictive skill.
@@ -9,22 +9,18 @@
 
 find_tp<-function(sgn, cutoff=0.05) {
 
-  best_tp <- list()
+  best_tp <- NULL
+  dirlst<-sort(unique(sgn$direction))
 
-  for (i in levels(sgn$direction)){
+  for (i in 1:length(dirlst)){
+    sbs<-which(sgn$pval[sgn$direction==dirlst[i]]<=cutoff)
+    if(length(sbs)==0) {
+      sbs<-which(sgn$tp[sgn$direction==dirlst[i]]==0)
+    } else {
+      sbs<-sbs[which.max(sgn$meanmaxL[sgn$direction==dirlst[i]][sbs])]
 
-    for (j in length(sgn$pval)){
-
-      if(sgn$pval[j] <= cutoff) {
-
-        best_tp[[i]]=sgn$tp[which.min(sgn$pval)]
-
-      } else {
-
-        best_tp[[i]] = 0
-
-      }
     }
+    best_tp<-rbind(best_tp, sgn[sgn$direction==dirlst[i],][sbs,])
   }
 
   return(best_tp)
