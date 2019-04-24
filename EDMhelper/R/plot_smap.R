3f73be0afee0de623920fb0157a34d9fa301d061
atclark <adam.tclark@gmail.com>
2019-04-24 11:38:25 +0200

    Updated to incorporate plot_simplex

--- EDMhelper/R/plot_smap.R
+++ EDMhelper/R/plot_smap.R
@@ -10,11 +10,11 @@
 #' @export
 
 plot_smap<-function(smap_out, predtype="rho", pname=NA) {
-  
+
   if(sum(grep("const_p_val", names(smap_out)))==0) {
     dm<-dim(smap_out[[1]])
     lng<-length(smap_out)
-    if(!is.na(pname)) {
+    if(!is.na(pname[1])) {
       nms<-pname
     } else {
       nms<-names(smap_out)
@@ -22,15 +22,15 @@ plot_smap<-function(smap_out, predtype="rho", pname=NA) {
     smap_out<-do.call("rbind", smap_out)
     rownames(smap_out)<-NULL
     smap_out$variable<-rep(nms, each=dm[1])
-      
+
   } else {
     smap_out$variable<-pname
   }
   variable<-smap_out$variable
-  
+
   theta<-smap_out$theta
-  
-  
+
+
   nice_theme<-theme_bw()+
     theme(axis.text=element_text(size=12),
           axis.title=element_text(size=14,face="bold"),
@@ -41,14 +41,14 @@ plot_smap<-function(smap_out, predtype="rho", pname=NA) {
           plot.background = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())
-  
+
   predtype<-smap_out[,predtype]
-  
+
   plot_ccm <- ggplot(smap_out, aes(x=theta, y=predtype))+
     geom_line(color="darkred")+
     facet_wrap(~variable)+
-    labs(y="Prediction skill", x="Library size")+
+    labs(y="Prediction skill", x="Nonlinearity, Theta")+
     nice_theme
-    
+
   return(plot_ccm)
 }
