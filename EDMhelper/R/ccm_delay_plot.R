5fd8ad64c9aff9dabc664c935601f70bbc99dbf0
canankarakoc <canankarakoc@gmail.com>
2019-04-25 10:35:08 +0200

    modified the delay plot

--- EDMhelper/R/ccm_delay_plot.R
+++ EDMhelper/R/ccm_delay_plot.R
@@ -22,21 +22,26 @@ ccm_delay_plot <- function (summaryout){
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())
 
-  summaryout_maxlibsize <-  summaryout[which(summaryout$lib_size==max(summaryout$lib_size)),]
-
-  tp         <- summaryout_maxlibsize$tp
-  Q50.       <- summaryout_maxlibsize$Q50.
-  Q15.86553. <- summaryout_maxlibsize$Q15.86553.
-  Q84.13447. <- summaryout_maxlibsize$Q84.13447.
+  summaryout_maxminlibsize <-  summaryout[which(summaryout$lib_size==max(summaryout$lib_size)
+                                          | summaryout$lib_size==min(summaryout$lib_size)),]
 
 
-  plot_ccm_delay <- ggplot(summaryout_maxlibsize, aes(x=tp, y=Q50.))+
+  tp         <- summaryout_maxminlibsize$tp
+  lib_size   <- summaryout_maxminlibsize$lib_size
+  Q50.       <- summaryout_maxminlibsize$Q50.
+  Q15.86553. <- summaryout_maxminlibsize$Q15.86553.
+  Q84.13447. <- summaryout_maxminlibsize$Q84.13447.
+
+
+  plot_ccm_delay <- ggplot(summaryout_maxminlibsize, aes(x=tp, y=Q50., group=as.factor(lib_size)))+
     geom_hline(yintercept=0, linetype="dashed")+
+    geom_vline(xintercept=0, linetype="dashed")+
     geom_ribbon(aes(ymin=Q15.86553., ymax=Q84.13447.),alpha=0.1)+
-    geom_line(color="darkred")+
+    geom_line(aes(color=as.factor(lib_size)))+
     facet_grid(~direction)+
     labs(y="Prediction skill", x="tp")+
     scale_x_continuous(breaks = tp)+
+    scale_color_manual(name="Lib. size", values=c("darkblue", "darkred"))+
     nice_theme
 
   return(plot_ccm_delay)
