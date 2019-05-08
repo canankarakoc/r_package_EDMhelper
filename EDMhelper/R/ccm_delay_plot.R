3fc64c20298a61c0c3cb0640b47bd3972a57a6bd
atclark <adam.tclark@gmail.com>
2019-05-08 15:16:50 +0300

    also reduced plot text size

--- EDMhelper/R/ccm_delay_plot.R
+++ EDMhelper/R/ccm_delay_plot.R
@@ -16,8 +16,8 @@ ccm_delay_plot <- function (summaryout){
           axis.title = element_text(size=14,face="bold"),
           legend.text = element_text(size=12),
           legend.title = element_text(size=12,face="bold"),
-          plot.title = element_text(size=14, face="bold"),
-          strip.text = element_text(size=14, face="bold"),
+          plot.title = element_text(size=12, face="bold"),
+          strip.text = element_text(size=12, face="bold"),
           plot.background = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank())
